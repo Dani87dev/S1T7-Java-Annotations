0634a4c53382fd38f888c47b47c2def4d3304610
Daniel Vilà <danivila.dev@gmail.com>
2026-01-14 13:27:24 +0100

    feat: create structure for exercice1 level2

---
+++ src/main/java/n2exercice1/Main.java
@@ -0,0 +1,23 @@
+package n2exercice1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        En aquest nivell faràs un pas més enllà creant la teva pròpia anotació.
+         Aquesta anotació servirà per indicar que un objecte Java s’ha de serialitzar en format JSON,
+         i rebrà com a paràmetre el directori de destinació. Començaràs a veure com les anotacions no només informen,
+         sinó que poden afectar la lògica del programa quan s’interpreten amb eines addicionals.
+
+        Objectiu: Comprendre com es poden definir annotations personalitzades amb paràmetres per enriquir la funcionalitat del codi.
+
+        Exercici 1 - JSON Serialization
+        Crea una anotació personalitzada que ha de permetre serialitzar un objecte Java en un fitxer JSON.
+        L’anotació ha de rebre el directori on es col·locarà el fitxer resultant.
+         */
+
+        
+
+    }
+}
